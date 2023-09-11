fb3d92550d01e4438afde8f14fbfc909f2d3ee4e
Roman Shpis <robi@MacBook-Pro-Roman.local>
2023-09-11 19:30:14 +0200

    add mobile logo and responsive navbar with animated menu_btn

--- terra-nova/src/components/header/Header.jsx
+++ terra-nova/src/components/header/Header.jsx
@@ -1,23 +1,29 @@
-import logo from './../../img/icons/Logo_m.svg'
+import logo from './../../img/icons/Logo.svg'
+import logo_mobile from './../../img/icons/Logo_mobile.svg'
 import './header.css'
+import React, {useState} from 'react'
 
 function Header() {
-    return(
+
+    const [open, setOpen] = useState(false);
+        return(
         <header className='header'>
-            <div className="header_bg">
-                <div className="container">
-                    <div className="header_row">
-                        <div className="header_logo">
-                            <img src={logo} alt="Logo" />
-                        </div>
-                        <div className="header_nav">
-                            <ul>
-                                <li><a href="#!">О нас</a></li>
-                                <li><a href="#!">Команда</a></li>
-                                <li><a href="#!">Цены</a></li>
-                                <li><a href="#!">Контакты</a></li>
-                            </ul>
-                        </div>
+            <div className="container">
+                <div className="header_row">
+                    <div className="header_logo">
+                        <img src={logo} alt="Logo" className="logo"/>
+                        <img src={logo_mobile} alt="Logo" className="logo_mobile" />
+                     </div>
+                    <div className={`header_nav ${open? "active" : "inactive"}`}>
+                        <a href="#!">О нас</a>
+                        <a href="#!">Команда</a>
+                        <a href="#!">Цены</a>
+                        <a href="#!">Контакты</a>
+                    </div>
+                    <div className={`menu_btn ${open? "clicked" : "unclicked"}`} onClick={()=>{setOpen(!open)}}>
+                        <span></span>
+                        <span></span>
+                        <span></span>
                     </div>
                 </div>
             </div>  
