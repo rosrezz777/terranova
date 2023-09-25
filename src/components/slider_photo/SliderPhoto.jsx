a68733657ebeb851b495653ed08c07f63a3bbab0
Roman Shpis <robi@MacBook-Pro-Roman.local>
2023-09-25 19:02:15 +0200

    fix props

--- src/components/slider_photo/SliderPhoto.jsx
+++ src/components/slider_photo/SliderPhoto.jsx
@@ -26,7 +26,7 @@ const SliderPhoto = ({ slides }) => {
       {SliderData.map((slide, index) => {
         return (
             index === current && (
-              <img src={slide.image} alt='gym' className='gym_photo' />
+              <img src={slide.gym_phot} alt='gym' className='gym_photo' />
             )
         );
       })}
